fcf47208ed38fe9f1c9d82ca980efd5a8e9466a6
Arthur Joppart <arthur.joppart@gmail.com>
2025-02-08 23:44:18 +0100

    feat: add category model

---
+++ internal/models/category.go
@@ -0,0 +1,18 @@
+package models
+
+type CategoryFields struct {
+	Name    string `json:"name" db:"name"`
+	GroupID int    `json:"group_id" db:"group_id"`
+}
+
+type Category struct {
+	WithID
+	WithOwnerID
+	CategoryFields
+	Metadata
+}
+
+type CategoryFieldsWithID struct {
+	WithID
+	CategoryFields
+}
