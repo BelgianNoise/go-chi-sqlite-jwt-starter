13e69f999a8ea71ab5f0e7625901029bd7f00e69
Arthur Joppart <arthur.joppart@gmail.com>
2025-02-09 15:07:38 +0100

    fix: typo

--- internal/user/service/user.service.go
+++ internal/user/service/user.service.go
@@ -6,7 +6,7 @@ import (
 
 type UserService interface {
 	ListUsers() ([]models.User, error)
-	CreateUser(category models.UserFields) (models.User, error)
+	CreateUser(user models.UserFields) (models.User, error)
 	GetUser(userID int64) (models.User, error)
 	UpdateUser(user models.User) (models.User, error)
 	DeleteUser(userID int64) error
