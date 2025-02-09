c9bd112b506cf01917e92fc7a4ccfc96b2349245
Arthur Joppart <arthur.joppart@gmail.com>
2025-02-09 15:48:17 +0100

    feat: make deletedAt optional

--- internal/models/metadata.go
+++ internal/models/metadata.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Metadata struct {
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at" db:"deleted_at"`
+	CreatedAt time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at" db:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty" db:"deleted_at"`
 }
