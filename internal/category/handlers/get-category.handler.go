0b212b4c09ac9c75473ef1f7a26eb3bd654110d1
Arthur Joppart <arthur.joppart@gmail.com>
2025-02-09 02:47:11 +0100

    feat: implement GET /category/id

--- internal/category/handlers/get-category.handler.go
+++ internal/category/handlers/get-category.handler.go
@@ -1,7 +1,13 @@
 package category_handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func GetCategory(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get Category"))
+	category := getCategoryFromContext(w, r)
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(category)
 }
